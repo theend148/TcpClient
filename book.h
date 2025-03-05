dcfdf3ea0e0289a0af366eb9ab6fa2a9412a2ad1
chen <hello@chen.com>
2025-03-05 22:20:03 +0800

    存档

--- book.h
+++ book.h
@@ -1,30 +1,39 @@
 #ifndef BOOK_H
 #define BOOK_H
 
+#include <QFile>
+#include <QHBoxLayout>
+#include <QListWidgetItem>
+#include <QPushButton>
+#include <QQueue>
+#include <QTimer>
+#include <QVBoxLayout>
 #include <QWidget>
-#include<QListWidgetItem>
-#include<QPushButton>
-#include<QVBoxLayout>
-#include<QHBoxLayout>
-#include"protocol.h"
-#include<QTimer>
-#include<QFile>
+#include <QtCore/QCryptographicHash>
 
-class Book : public QWidget
-{
+#include "protocol.h"
+
+struct FileChunk {
+    qint64 index;
+    qint64 offset;
+    qint64 size;
+    bool uploaded;
+};
+
+class Book : public QWidget {
     Q_OBJECT
 public:
-    explicit Book(QWidget *parent = 0);
-    void updateDirList(const PDU *pdu);
+    explicit Book(QWidget* parent = 0);
+    void updateDirList(const PDU* pdu);
     QString getEnterPath();
-    void setDownloadStatus(bool status);//设置下载状态
+    void setDownloadStatus(bool status);  // 设置下载状态
     bool getDownloadStatus();
     QString getSaveFilePath();
-    void updateLocalDownloadFileName();//更新本地下载文件的文件名称
+    void updateLocalDownloadFileName();  // 更新本地下载文件的文件名称
     QString getShareFileName();
-    qint64 m_iTotal;//下载文件的总字节大小
-    qint64 m_iRevice;//下载文件目前接受到文件的大小
-    QFile m_pFile;//用于本地下载文件使用
+    qint64 m_iTotal;   // 下载文件的总字节大小
+    qint64 m_iRevice;  // 下载文件目前接受到文件的大小
+    QFile m_pFile;     // 用于本地下载文件使用
 
 signals:
 
@@ -33,7 +42,7 @@ public slots:
     void flushDir();
     void delDir();
     void renameDir();
-    void enterDir(const QModelIndex &index);
+    void enterDir(const QModelIndex& index);
     // 返回上一级
     void returnPre();
     // 上传文件-预先发送消息
@@ -43,23 +52,36 @@ public slots:
     void delFile();
     void downloadFile();
     void shareFile();
+    void initializeUpload();
+    void processUploadQueue();
+    QString calculateFileMD5(const QString& filePath);
+    void createUploadQueue();
+
 private:
-    QListWidget *m_pBookListW;
-    QPushButton *m_pReturnPB;
-    QPushButton *m_pCreateDirPB;
-    QPushButton *m_pDelDirPB;
-    QPushButton *m_pRenameDirPB;
-    QPushButton *m_pFlushDirPB;
-    QPushButton *m_pUploadFilePB;
-    QPushButton *m_pDelFilePB;
-    QPushButton *m_pDownloadFilePB;
-    QPushButton *m_pShareFilePB;
-    QString m_enterPath; //进入文件夹的路径
-    QString m_strUploadFilePath;//上传文件的路径
-    QTimer *m_pTimer;//定时器执行上传文件，【防止发送文件数据过快导致粘包】
-    QString m_strSaveFilePath;//保存文件的路径
-    bool m_pDownload;//是否处于文件下载状态
+    QListWidget* m_pBookListW;
+    QPushButton* m_pReturnPB;
+    QPushButton* m_pCreateDirPB;
+    QPushButton* m_pDelDirPB;
+    QPushButton* m_pRenameDirPB;
+    QPushButton* m_pFlushDirPB;
+    QPushButton* m_pUploadFilePB;
+    QPushButton* m_pDelFilePB;
+    QPushButton* m_pDownloadFilePB;
+    QPushButton* m_pShareFilePB;
+    QString m_enterPath;          // 进入文件夹的路径
+    QString m_strUploadFilePath;  // 上传文件的路径
+    QString m_strSaveFilePath;    // 保存文件的路径
+    bool m_pDownload;             // 是否处于文件下载状态
     QString m_shareFileName;
+    QString m_strFileMD5;
+
+public:
+    QQueue<FileChunk> m_uploadQueue;
+    QVector<qint64> m_uploadedChunks;
+    static qint64 CHUNK_SIZE;  // 2MB
+    qint64 m_totalFileSize;
+    QTimer* m_pTimer;  // 定时器执行上传文件，【防止发送文件数据过快导致粘包】
 };
 
-#endif // BOOK_H
+
+#endif  // BOOK_H
